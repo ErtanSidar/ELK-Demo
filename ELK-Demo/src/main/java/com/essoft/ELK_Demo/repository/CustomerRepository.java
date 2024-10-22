e5d032fc574c3c4c7d8038898b23556f197bb1b9
ErtanSidar <essoft3@gmail.com>
2024-10-22 16:57:30 +0300

    add repository query

--- ELK-Demo/src/main/java/com/essoft/ELK_Demo/repository/CustomerRepository.java
+++ ELK-Demo/src/main/java/com/essoft/ELK_Demo/repository/CustomerRepository.java
@@ -1,8 +1,28 @@
 package com.essoft.ELK_Demo.repository;
 
 import entity.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.elasticsearch.annotations.Query;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.scheduling.annotation.Async;
+
+import java.util.List;
+import java.util.concurrent.Future;
+import java.util.stream.Stream;
 
 public interface CustomerRepository extends ElasticsearchRepository<Customer, String> {
     Iterable<Customer> findByLastName(String lastName);
+
+    List<Customer> findAllByNameAndSurname(String name, String surname);
+
+    @Query("select c from Customer c")
+    Stream<Customer> findAllCustomerStream();
+
+    Page<Customer> findAllByNameLike(String name, Pageable pageable);
+
+    @Async
+    Future<Customer> findAllByName(String name);
+
+    Page<Customer> findAllByNameContaining(String name, Pageable pageable);
 }
